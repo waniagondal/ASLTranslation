bf39a4c95e3de44d4196d26bc87e229a42df36ff
Gavin Song <gavin.song@mail.utoronto.ca>
2024-11-04 10:44:13 -0500

    added instance variables and getters to the initial entities

--- src/main/java/entity/AudioOutput.java
+++ src/main/java/entity/AudioOutput.java
@@ -1,3 +1,10 @@
 package entity;
 public class AudioOutput {
+    private final String text;
+    private final String audioFormat;
+    private final String language;
+
+    AudioOutput (String text, String audioFormat, String language) {
+
+    }
 }
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,19 @@
 package entity;
 
 public class User {
+    private final String userID;
+    private final String languagePref;
+
+    User(String userID, String languagePref) {
+        this.userID = userID;
+        this.languagePref = languagePref;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getLanguagePref() {
+        return languagePref;
+    }
 }
