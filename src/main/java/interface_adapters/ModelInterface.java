0d987fdcfd16c6b48d1aca9c8637655377b96af7
F4NG66 <fang.sheng@mail.utoronto.ca>
2024-11-10 13:53:20 -0500

    Integrate ModelPredictor with Gesture and Translation for prediction display

--- src/main/java/interface_adapters/ModelInterface.java
+++ src/main/java/interface_adapters/ModelInterface.java
@@ -1,5 +1,4 @@
 package interface_adapters;
-
 public interface ModelInterface {
     /**
      * receive input data and return model prediction
@@ -7,5 +6,5 @@ public interface ModelInterface {
      * @param input asl input data, e.g. hand landmark
      * @return result of prediction
      */
-    String predict(HandLandmark input);
+    String predict(Gesture input);
 }
--- src/main/java/interface_adapters/ModelPredictor.java
+++ src/main/java/interface_adapters/ModelPredictor.java
@@ -1,6 +1,6 @@
 package interface_adapters;
 
-import entity.HandLandmark;
+import entity.Gesture;
 import frameworks_and_drivers.ModelService;
 
 public class ModelPredictor implements ModelInterface {
@@ -8,13 +8,22 @@ public class ModelPredictor implements ModelInterface {
     private ModelService modelService;
 
     public ModelPredictor(ModelService modelService) {
+
         this.modelService = modelService;
     }
 
     @Override
-    public String predict(HandLandmark input) {
-        float[] modelInput = InputPreprocessor.preprocess(input);
+    public String predict(Gesture gesture) {
+        float[] modelInput = InputPreprocessor.preprocess(gesture.getGestureCoordinates());
         float[] rawOutput = modelService.predict(modelInput);
         return OutputPostprocessor.postprocess(rawOutput);
+    }
+
+    public Translation getPredictionTranslation(Gesture gesture, String targetLanguage) {
+        String predictionText = predict(gesture);
+        List<String> translationHistory = new ArrayList<>();
+        translationHistory.add(predictionText);
+
+        return new Translation(predictionText, targetLanguage, translationHistory);
     }
 }
