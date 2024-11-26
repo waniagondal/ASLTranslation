505a26029e77505f8006ba892118c22c60334f83
F4NG66 <fang.sheng@mail.utoronto.ca>
2024-11-26 15:17:22 -0500

    test for speech to text

--- src/test/java/entities/AudioTranscriptionTest.java
+++ src/test/java/entities/AudioTranscriptionTest.java
@@ -1,3 +1,5 @@
+package entities;
+
 import entity.AudioTranscription;
 import org.junit.jupiter.api.Test;
 
@@ -7,19 +9,15 @@ class AudioTranscriptionTest {
 
     @Test
     void testAudioTranscriptionCreation() {
-        // Arrange
         String transcribedText = "This is a test transcription";
 
-        // Act
         AudioTranscription transcription = new AudioTranscription(transcribedText);
 
-        // Assert
         assertEquals(transcribedText, transcription.getText());
     }
 
     @Test
     void testGetText() {
-        // Arrange
         String transcribedText = "Another test transcription";
         AudioTranscription transcription = new AudioTranscription(transcribedText);
 
